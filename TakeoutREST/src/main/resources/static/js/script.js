062a11f0533b5651b36b828dc50da4168072fbdd
Max <maxgoduco@gmail.com>
2022-06-12 22:46:43 -0400

    broke update everything else works

--- TakeoutREST/src/main/resources/static/js/script.js
+++ TakeoutREST/src/main/resources/static/js/script.js
@@ -84,7 +84,18 @@ function displayFoodList(foodList) {
 		td = document.createElement('td');
 		td.textContent = food.rating;
 		tr.appendChild(td);
-		tr.addEventListener('click', showFoodDetails);
+	let update = document.createElement('button');
+	update.textContent = 'Update';
+	let remove = document.createElement('button');
+	remove.textContent = 'Delete';
+	update.addEventListener('click', function(e) {
+		updateFood(food);
+	});
+	remove.addEventListener('click', function(e) {
+		deleteFood(food);
+	});
+	tbody.appendChild(update);
+	tbody.appendChild(remove);
 	}
 }
 
@@ -109,7 +120,7 @@ function updateFood(food) {
 	tbody.textContent = '';
 	let updateFoodForm = document.createElement('form');
 	updateFoodForm.setAttribute('name', 'updateFoodForm');
-	let div = document.getElementById('tableFormDiv');
+	let div = document.getElementById('foodTypeDiv');
 
 	let foodName = document.createElement('label');
 	foodName.textContent = "Food Name: "
@@ -119,6 +130,46 @@ function updateFood(food) {
 	updateFoodForm.appendChild(foodName);
 	updateFoodForm.appendChild(foodNameInput);
 	updateFoodForm.appendChild(document.createElement('br'));
+	
+	let foodDescription = document.createElement('label');
+	foodDescription.textContent = "Food Description: "
+	let foodDescriptionInput = document.createElement('input');
+	foodDescriptionInput.setAttribute('description', 'foodDescription');
+	foodDescriptionInput.setAttribute('value', food.description);
+	updateFoodForm.appendChild(foodDescription);
+	updateFoodForm.appendChild(foodDescriptionInput);
+	updateFoodForm.appendChild(document.createElement('br'));
+	foodDescription.textContent = "Food Description: "
+	
+	let foodPrice = document.createElement('label');
+	foodPrice.textContent = "Food Price $: "
+	let foodPriceInput = document.createElement('input');
+	foodPriceInput.setAttribute('price', 'foodPrice');
+	foodPriceInput.setAttribute('value', food.price);
+	updateFoodForm.appendChild(foodPrice);
+	updateFoodForm.appendChild(foodPriceInput);
+	updateFoodForm.appendChild(document.createElement('br'));
+	
+	
+	let foodDate = document.createElement('label');
+	foodDate.textContent = "Food Date: 2022-05-04T02:00:00 "
+	let foodDateInput = document.createElement('input');
+	foodDateInput.setAttribute('purchaseDate', 'foodDate');
+	foodDateInput.setAttribute('value', food.purchaseDate);
+	updateFoodForm.appendChild(foodDate);
+	updateFoodForm.appendChild(foodDateInput);
+	updateFoodForm.appendChild(document.createElement('br'));
+	
+	
+	let foodRating = document.createElement('label');
+	foodRating.textContent = "Food Rating: "
+	let foodRatingInput = document.createElement('input');
+	foodRatingInput.setAttribute('rating', 'foodRating');
+	foodRatingInput.setAttribute('value', food.rating);
+	updateFoodForm.appendChild(foodRating);
+	updateFoodForm.appendChild(foodRatingInput);
+	updateFoodForm.appendChild(document.createElement('br'));
+	
 
 	let button = document.createElement('button');
 	button.textContent = "Update";
@@ -126,12 +177,12 @@ function updateFood(food) {
 	button.addEventListener('click', function(e) {
 		e.preventDefault();
 
-		let updateFood = {
-			name: form.name.value,
-			description: form.description.value,
-			price: form.price.value,
-			purchaseDate: form.purchaseDate.value,
-			rating: form.rating.value
+	let updateFood = {
+			name: updateFoodForm.name,
+			description: updateFoodForm.description,
+			price: updateFoodForm.price,
+			purchaseDate: updateFoodForm.purchaseDate,
+			rating: updateFoodForm.rating
 		};
 		sendUpdateFood(updateFood);
 		updateFoodForm.textContent = '';
@@ -139,6 +190,24 @@ function updateFood(food) {
 	updateFoodForm.appendChild(button);
 	div.appendChild(updateFoodForm);
 
+}
+
+function sendUpdateFood(food) {
+	let xhr = new XMLHttpRequest();
+	//xhr.open('PUT', 'api/food');
+	xhr.open('PUT', `api/food/${food.id}`);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState === 4) {
+			if (xhr.status < 200) {
+				let food = JSON.parse(xhr.responseText);
+				console.log('Updated food');
+			} else {
+				console.log('Error updating food: ' + xhr.status);
+			}
+		}
+	};
+	xhr.setRequestHeader("Content-type", "application/json");
+	xhr.send(JSON.stringify(food));
 }
 
 function getFood(foodId) {
@@ -166,30 +235,26 @@ function displayError(message) {
 function showFoodDetails(food) {
 	let tbody = document.getElementById('foodTable');
 	tbody.textContent = '';
-	let fname = document.createElement('p');
-	let fdescription = document.createElement('p');
-	let fprice = document.createElement('p');
-	let fdate = document.createElement('p');
-	let frating = document.createElement('p');
-	fname.textContent = "Takeout Order: " + food.name;
-	fdescription.textContent = "Description: " + food.description;
-	fprice.textContent = "Price ($): " + food.price;
-	fdate.textContent = "Date Purchased: " + food.date;
-	frating.textContent = "Rating: " + food.rating;
-
 	let tr = document.createElement('tr');
 	tbody.appendChild(tr);
 	let td = document.createElement('td');
-	td.appendChild(fname);
-	td.appendChild(document.createElement('td'));
-	td.appendChild(fdescription);
-	td.appendChild(document.createElement('td'));
-	td.appendChild(fprice);
-	td.appendChild(document.createElement('td'));
-	td.appendChild(fdate);
-	td.appendChild(document.createElement('td'));
-	td.appendChild(frating);
-	td.appendChild(document.createElement('td'));
+	td.textContent = food.id;
+	tr.appendChild(td);
+	td = document.createElement('td');
+	td.textContent = food.name;
+	tr.appendChild(td);
+	td = document.createElement('td');
+	td.textContent = food.description;
+	tr.appendChild(td);
+	td = document.createElement('td');
+	td.textContent = food.price;
+	tr.appendChild(td);
+	td = document.createElement('td');
+	td.textContent = food.purchaseDate;
+	tr.appendChild(td);
+	td = document.createElement('td');
+	td.textContent = food.rating;
+	tr.appendChild(td);
 
 	let update = document.createElement('button');
 	update.textContent = 'Update';
